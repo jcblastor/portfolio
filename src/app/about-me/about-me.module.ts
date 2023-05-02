ffa5fca8e90d386ccb0bba82f874112fa2914c8c
Carlos Guzmán <jcguzmanblack@gmail.com>
2023-05-02 12:17:45 -0300

    profile page added, and responsive done.

--- src/app/about-me/about-me.module.ts
+++ src/app/about-me/about-me.module.ts
@@ -1,18 +1,21 @@
 import { NgModule } from "@angular/core";
 
 import { AboutMeRoutingModule } from "./about-me-routing.module";
-
+import { SharedModule } from "../shared/shared.module";
 
 import { ContactMeComponent } from "./pages/contact-me/contact-me.component";
 import { ProfileComponent } from "./pages/profile/profile.component";
+import { ButtonComponent } from "./components/button/button.component";
 
 @NgModule({
   declarations: [
     ProfileComponent,
+    ButtonComponent,
     ContactMeComponent,
   ],
   imports: [
     AboutMeRoutingModule,
+    SharedModule,
   ]
 })
 export class AboutMeModule { }
